ac5f1b3f80e8756f40bb0fc3eccd1e2cf61185da
zonej <jzone1229@gmail.com>
2014-02-26 10:32:52 -0500

    Prepped to switch to user authentication.

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,5 @@
 <?php 
 	$CONSUMER_KEY = "x4PfxII2udpnbuzETHdTqg";
 	$CONSUMER_SECRET = "qjlwSS9epW2tLotuc4ndWxiq83bcye8skWTXiRZ5vo8";
-	$ACCESS_TOKEN = "110624472-sNW01zMZaUJRnI6nQ1wra7ci2Gf8b3luhPs0q8xo";
-	$ACCESS_SECRET = "xJ4cvZcIZjkvRnt6tju2TmmcXq20P2XMRTIjxcTHlnwkR";
+	$OAUTH_CALLBACK = "oauth.php";
 	?>
